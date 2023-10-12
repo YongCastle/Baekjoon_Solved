e78b8bc59aa5c20351009b52b1b7ded416c99c08
Limyongseong <53935607+YongCastle@users.noreply.github.com>
2023-10-12 15:48:09 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,31 +1,14 @@
-#include <iostream>
-#include <bits/stdc++.h>
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#include <cstdio>
 
-
-int main()
-{
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0); std::cout.tie(0);
-    
-    int N; std::cin>>N;
-    int arr[301];
-    int DP[301][2];
-    
-    std::cin>>arr[0]>>arr[1];
-    DP[0][0] = arr[0];          //[i][0] : prev step is not Done
-    DP[0][1] = arr[0];          //[i][1] : prev step is Done
-    DP[1][0] = arr[1];
-    DP[1][1] = arr[0] + arr[1];
-    // DP[i, 1] = arr[i] + DP[i-1, 0]
-    // DP[i, 0] = arr[i] + max(DP[i-2, 0], DP[i-2, 1])
-    for(int i=2; i<N; i++){
-        std::cin >> arr[i];
-        DP[i][1] = arr[i] + DP[i-1][0];
-        DP[i][0] = arr[i] + MAX(DP[i-2][1], DP[i-2][0]);
-    }
-
-    std::cout << MAX(DP[N-1][1], DP[N-1][0]);
-    
-    return 0;
+int n, a[310], d[310];
+int max(int p, int q) {return p>q ? p:q;}
+int main() {
+	int i;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++) {
+		scanf("%d", &a[i]);
+		d[i] = max(d[i-2], d[i-3]+a[i-1]) + a[i];
+	}
+	printf("%d", d[n]);
+	return 0;
 }
