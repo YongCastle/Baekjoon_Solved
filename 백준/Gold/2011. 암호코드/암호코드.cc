68684b3a3dc862048f51f765215804d2e013a8a9
Limyongseong <53935607+YongCastle@users.noreply.github.com>
2023-10-14 02:20:17 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.cc
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -1,77 +1,21 @@
-#include <iostream>
-#define DIV 1000000
-
-//                                       <=       <=
-inline int charToInt(const char *arr, int start, int end){
-    int sum = 0;
-    for(int i=start; i<end+1; i++){
-        sum *= 10;
-        sum += int(arr[i]) - int('0');
-    }
-    return sum;
-};
-
-int DP[5001] = {0, };
-bool update(const char *password, const int start, const int end){
-    // start <= i <= end
-    DP[0] = 1;  //None
-
-    DP[1] = 1;  //DP[n] <-> password[n-1]
-    for(int i=2; i<end+1; i++){
-        if(password[i] == '0'){    
-            if(password[i-1] == '0'){
-                return false;
-            }  
-            if(password[i-1]<='2'){     // Case -  20  
-                DP[i] = DP[i-2];
-            }
-            else{                       // Case -  30
-                return false;
-            }
-            continue;
-        }
-        if(password[i-1] == '0'){    // Case - 03
-            DP[i] = DP[i-1];
-            continue;
-        }
-        int tmp = charToInt(password, i-1, i);
-        if(tmp-1<=25){
-            DP[i] = DP[i-1] + DP[i-2];
-        }
-        else{
-            DP[i] = DP[i-1];
-        }
-        DP[i] = DP[i] % DIV;
-    }
-    return true;
-};
+#include <stdio.h>
 
 int main(void){
-    char password[5002];
-    std::cin.getline(&password[1], sizeof(password)-sizeof(char));
-    password[0] = ' ';
-    if(password[1] == '0'){
-        std::cout<<0;
-        return 0;
-    }
-    int i=0;
-    while(password[i++]!='\0');
-    int start = 1;
-    int end = i-2;
-    if(!update(password, start, end)){
-        std::cout<<0;
-        return 0;
-    }
-    // std::cout<<'\n';
-    // for(int i=0; i<end+1; i++){
-    //     std::cout << password[i] <<' ';
-    // }
-    // std::cout<<'\n';
-    // for(int i=0; i<end+1; i++){
-    //     std::cout << DP[i] <<' ';
-    // }
-    // std::cout<<'\n';
-    std::cout<<DP[end];
-    
-    return 0;
+	int cp[5001]; // 암호
+	int dp[5001]={ 0, };
+	
+	int i=1;
+	dp[0]=1;
+
+	while(scanf("%1d", cp+i)!=EOF) {
+		if(cp[i]>0)
+			dp[i]=dp[i-1]%1000000;
+			
+		if(cp[i-1]==1||(cp[i-1]==2&&cp[i]<=6)) 
+			dp[i]=(dp[i]+dp[i-2])%1000000;
+			
+		i++;
+	}
+	
+	printf("%d\n", dp[i-1]%1000000);
 }
