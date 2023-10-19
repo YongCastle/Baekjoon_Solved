597ab9ab158b5ef020bc556537592677264ca856
Limyongseong <53935607+YongCastle@users.noreply.github.com>
2023-10-19 14:32:16 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(void){
+    int N;
+    scanf("%d", &N);
+    for(int i=2; i*i<=N; i++){
+        while(N%i==0){
+            printf("%d\n", i);
+            N/=i;
+        }
+    }
+    if(N>1)printf("%d\n",N);
+    return 0;
+}
